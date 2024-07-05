9a05d5c9ce63dc12de85895f62a8dbf5de542d92
itsyasin1 <mryasinn49@gmail.com>
2024-07-05 18:04:26 +0600

    Add files via upload

---
+++ Animal/Animal.java
@@ -0,0 +1,9 @@
+package Animal;
+
+public class Animal {
+    String name;
+    void eat()
+    {
+        System.out.println("I can eat Hooman");
+    }
+}
---
+++ Animal/Child.java
@@ -0,0 +1,8 @@
+package Animal;
+
+public class Child extends Animal{
+    void disPlay()
+    {
+        System.out.println("Name: " + name);//accessing name(instance variable) from parent class(Animal)
+    }
+}
