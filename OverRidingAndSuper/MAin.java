5e241e9e2cd4ae12921ae41d955107d6efdad315
itsyasin1 <mryasinn49@gmail.com>
2024-07-05 18:06:12 +0600

    Create MAin.java

---
+++ OverRidingAndSuper/MAin.java
@@ -0,0 +1,13 @@
+package OverRidingAndSuper;
+
+public class MAin {
+    public static void main(String[] args) {
+        // create an object of the subclass
+        Dog labrador = new Dog();
+
+        // call the eat() method
+        labrador.eat();
+        labrador.bark();
+    }
+}
+
