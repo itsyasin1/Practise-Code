12212efa5e02766a99a9b71302f583fca279863e
itsyasin1 <mryasinn49@gmail.com>
2024-07-05 18:05:18 +0600

    Create MAin.java

---
+++ Inheritense/MAin.java
@@ -0,0 +1,11 @@
+package Inheritense;
+
+public class MAin {
+    public static void main(String[] args) {
+        Child ch = new Child();
+        ch.NAme = "Mr";
+        ch.age = 18;
+        ch.qualifications = "Hello";
+        ch.displayInfo2();
+    }
+}
